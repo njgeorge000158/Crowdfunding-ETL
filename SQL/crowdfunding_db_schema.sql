0add26cb4b89315968b82210444c2b18a139d26d
N. James George <137228821+njgeorge000158@users.noreply.github.com>
2023-09-22 23:53:52 -0700

    Update crowdfunding_db_schema.sql

--- SQL/crowdfunding_db_schema.sql
+++ SQL/crowdfunding_db_schema.sql
@@ -28,26 +28,26 @@ CREATE TABLE
 	category
 	    (category_id VARCHAR(10),
 	     category VARCHAR(50) NOT NULL,
-            PRIMARY KEY (category_id));
+            	PRIMARY KEY (category_id));
 
 CREATE TABLE 
 	subcategory
 	    (subcategory_id VARCHAR(15),
 	     subcategory VARCHAR(50) NOT NULL,
-            PRIMARY KEY (subcategory_id));
+            	PRIMARY KEY (subcategory_id));
 
 CREATE TABLE 
     contacts 
         (contact_id SERIAL,
-	     first_name VARCHAR(40) NOT NULL,
+ 	 first_name VARCHAR(40) NOT NULL,
          last_name VARCHAR(40) NOT NULL,
          email VARCHAR(100) NOT NULL,
 	        PRIMARY KEY (contact_id));
 
 CREATE TABLE 
     campaign
-	    (cf_id SERIAL,
-	     contact_id SERIAL NOT NULL,
+	(cf_id SERIAL,
+	 contact_id SERIAL NOT NULL,
          company_name VARCHAR(100) NOT NULL,
          description TEXT NOT NULL,
          goal NUMERIC(12,2) NOT NULL,
